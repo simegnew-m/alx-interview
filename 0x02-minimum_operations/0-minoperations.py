dcf57ca751e2f1163ae943786fa8791b6606c28c
simegnew-m <simegnew.misganaw@gmail.com>
2022-06-30 00:11:17 -0700

    #0x02-minimum_operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
-'''
-calculates the minimum amount of processes necessary
-to create a certain number of characters.
-You must imagine that there is only one function
-to copy everything and one to paste.
+'''The minimum operations coding challenge.
 '''
 
 
 def minOperations(n):
-    p = 0
-
-    if n <= 1:
-        return p
-
-    for i in range(2, n + 1):
-        while (0 == n % i):
-            p = p + i
-            n = n / i
-            if n < i:
-                break
-    return p
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int):
+        return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return ops_count
